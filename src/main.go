9c984c8e7743beb9fd7d0e494f0c5660cbf937c8
Al Avery <al.avery.dev@gmail.com>
2021-02-10 06:27:03 -0600

    create server

--- src/main.go
+++ src/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
